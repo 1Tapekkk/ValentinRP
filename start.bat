82ef2dfe1f74f9fb5f92e0b562e1826a919e9d4a
1Tapekkk <83430417+1Tapekkk@users.noreply.github.com>
2022-06-01 20:56:41 +0200

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+title SouthSideRP
+color 5B
+echo                            1TapGod
+echo\
+echo                                                     Version 1.0
+echo\
+echo\
+echo   Use "CTRL+C" to stop the bot
+echo\
+echo *******************************************************************************************
+node index.js
